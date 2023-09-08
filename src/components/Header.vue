6c467c05b82ef930635e66102c1439e8d9789660
mozzarelladoublestick <92583850+mozzarelladoublestick@users.noreply.github.com>
2023-09-08 15:18:04 +0200

    mobile update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,22 +3,21 @@
 </script>
 
 <template>
-  <div class="header min-h-screen text-lg flex justify-center items-center text-white">
-    <p class="flex-row md:flex-col p-12">PEOPLE</p>
-    <p class="flex-row md:flex-col p-12">OUTDOOR</p>
-    <p class="flex-row md:flex-col p-12">LOGO</p>
-    <p class="flex-row md:flex-col p-12">ABOUT</p>
-    <p class="flex-row md:flex-col p-12">CONTACT</p>
+  <div class="header sm: bg-contain md:bg-cover min-h-screen text-lg text-center md:flex justify-center items-center text-white">
+    <p class="sm:py-22 flex-col p-12">PEOPLE</p>
+    <p class=" flex-col p-12">OUTDOOR</p>
+    <p class="flex-col p-12">LOGO</p>
+    <p class="flex-col p-12">ABOUT</p>
+    <p class=" flex-col p-12">CONTACT</p>
   </div>
 </template>
 
 <style scoped>
 .header{
-  background-image: url("../public/cover.jpg");
-  background-size: cover;
+  background-image: url("../assets/cover/cover.jpg");
 }
 
 @media (min-width: 768px) {
-  
+
 }
 </style>
