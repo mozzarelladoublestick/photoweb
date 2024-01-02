03d788c5125dccf8f45ff823317b4207bd47dd97
mozzarelladoublestick <92583850+mozzarelladoublestick@users.noreply.github.com>
2024-01-02 11:21:33 +0100

    new logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-      <div class="absolute z-10 text-white top-2 left-4"><a href="/" class="logo"><img class="h-16 w-16 mr-2" src="../assets/logo.svg"></a></div>
+      <div class="absolute z-10 text-white top-2 left-4"><a href="/" class="logo"><img class="h-16 w-16 mr-2" src="../assets/logo_white_square.svg"></a></div>
       <div class="z-10 absolute text-white top-2 right-4"><div class="inline-flex"> <router-link to="/outdoor"><h4 class="px-6">outdoor</h4></router-link>    <router-link to="/people"><h4 class="px-6">people</h4></router-link><router-link to="/about"><h4 class="px-6">me</h4></router-link></div></div>
 </template>
 
